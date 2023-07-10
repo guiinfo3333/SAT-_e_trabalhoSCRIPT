e4c22fef920c822da85e2ed6328cb44eab410da4
Guilherme <antonioguilhermeinfo@gmail.com>
2023-07-10 10:36:16 -0300

    send

--- trabalhoSCRIPT/index.js
+++ trabalhoSCRIPT/index.js
@@ -1,31 +1,41 @@
 const express = require('express');
-const { exec } = require('child_process');
-
-const app = express();
+const vm = require('vm');
 const cors = require('cors');
 
+const app = express();
 app.use(express.json());
 app.use(cors());
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+  res.render('../views/index.ejs');
+});
 
 app.post('/execute', (req, res) => {
   const code = req.body.code;
   console.log(code)
-  
-  exec(code, (error, stdout, stderr) => {
-    console.log(error)
-    if (error) {
-      res.status(500).json({ error: error.message });
-      return;
-    }
 
-    if (stderr) {
-      res.status(200).json({ output: stderr });
-      return;
+  // Cria um contexto de execução isolado
+  const sandbox = { console: {} };
+  sandbox.console.log = function (message) {
+    // Armazena a saída do console em uma variável para retornar ao cliente
+    if (!sandbox.output) {
+      sandbox.output = message;
+    } else {
+      sandbox.output += '\n' + message;
     }
+  };
 
-    res.status(200).json({ output: stdout });
-  });
+  try {
+    // Executa o código no contexto isolado
+    vm.createContext(sandbox);
+    vm.runInContext(code, sandbox);
+
+    const output = sandbox.output || '';
+    res.status(200).json({ output });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const port = 3000;
